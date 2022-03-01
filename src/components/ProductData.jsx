592c3f675df8c4ffba37317207912f10ee7f8bb7
Ali G.Jahed <aligjahed@gmail.com>
2022-03-01 12:37:53 -0500

    add cart button position fixed

--- src/components/ProductData.jsx
+++ src/components/ProductData.jsx
@@ -32,8 +32,8 @@ const ProductData = ({ addCart }) => {
         </div>
         <p className="font-bold text-Grayish-blue line-through ">$250.00</p>
       </div>
-      <div className="">
-        <div className="flex justify-between items-center bg-Light-grayish-blue px-[24px] py-[22px] rounded-[8px] mb-[16px]">
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center w-full">
+        <div className="flex justify-between items-center bg-Light-grayish-blue px-[24px] py-[22px] rounded-[8px] mb-[16px] lg:w-full lg:mb-0">
           <img
             className="cursor-pointer"
             onClick={() => {
@@ -53,12 +53,12 @@ const ProductData = ({ addCart }) => {
           />
         </div>
         <div
-          className="flex justify-center items-center px-[90px] py-[20px] bg-Orange rounded-[8px] lg:hover:opacity-50 cursor-pointer"
+          className="flex justify-center items-center px-[90px] py-[20px] bg-Orange rounded-[8px] lg:hover:opacity-50 cursor-pointer lg:h-[55px] lg:w-[270px] lg:px-[75px]"
           onClick={() => {
             productCount !== 0 && setCartData(productCount);
           }}
         >
-          <svg className="mr-[14px]" width="22" height="20">
+          <svg className="mr-[14px] lg:m-0" width="50" height="20">
             <path
               fill="#FFFFFF"
               d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
